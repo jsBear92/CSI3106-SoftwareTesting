0743d653eaa6d15b9435cd549ded3c2b4f910f77
mikebernal <34907757+mikebernal@users.noreply.github.com>
2022-04-19 01:30:51 +0800

    added Room class DI resolver

--- tests/RoomParameterResolver.java
+++ tests/RoomParameterResolver.java
@@ -5,21 +5,21 @@ import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 
-import au.edu.sccs.csp3105.NBookingPlanner.Planner;
+import au.edu.sccs.csp3105.NBookingPlanner.Room;
 
-public class PlannerParameterResolver implements ParameterResolver {
+public class RoomParameterResolver implements ParameterResolver {
 	
 	@Override
 	public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
 		// TODO Auto-generated method stub
 		
-		return parameterContext.getParameter().getType() == Planner.class;
+		return parameterContext.getParameter().getType() == Room.class;
 	}
 
 	@Override
 	public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
 		// TODO Auto-generated method stub
-		return new Planner();
+		return new Room();
 	}
 
 }
--- tests/ScheduleMeetingTest.java
+++ tests/ScheduleMeetingTest.java
@@ -5,21 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import au.edu.sccs.csp3105.NBookingPlanner.ConflictsException;
 import au.edu.sccs.csp3105.NBookingPlanner.Room;
 
+@ExtendWith(RoomParameterResolver.class)
 @DisplayName("Schedule a meeting test")
 public class ScheduleMeetingTest {
 	  @Test
 	  @DisplayName("Day is greater than 30")
-	  public void testIncorrectDay() {
-		  Room where = new Room();
-		  Exception exception = assertThrows(ConflictsException.class, ()-> where.isBusy(5, 503, 3, 4));
-		  
+	  public void testIncorrectDay(Room where) {
+		  Exception exception = assertThrows(ConflictsException.class, ()-> where.isBusy(5, 530, 3, 4));
 		  String expectedMessage = "Day does not exist.";
 		  String actualMessage   = exception.getMessage();
-		  
 		  assertTrue(actualMessage.contains(expectedMessage));
 	  }
 
