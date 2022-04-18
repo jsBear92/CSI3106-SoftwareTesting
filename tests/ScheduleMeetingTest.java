902383fb5240d8f0feee08806143a2163127e066
mikebernal <34907757+mikebernal@users.noreply.github.com>
2022-04-19 00:49:01 +0800

    refactorr testIncorrectDay test case

--- tests/ScheduleMeetingTest.java
+++ tests/ScheduleMeetingTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
@@ -10,11 +11,16 @@ import au.edu.sccs.csp3105.NBookingPlanner.Room;
 
 @DisplayName("Schedule a meeting test")
 public class ScheduleMeetingTest {
-  @Test
-  @DisplayName("Day is greater than 30")
-  public void testIncorrectDay() {
-	  Room where = new Room();
-	  assertThrows(ConflictsException.class, ()-> where.isBusy(5, 503, 3, 4));
-  }
+	  @Test
+	  @DisplayName("Day is greater than 30")
+	  public void testIncorrectDay() {
+		  Room where = new Room();
+		  Exception exception = assertThrows(ConflictsException.class, ()-> where.isBusy(5, 503, 3, 4));
+		  
+		  String expectedMessage = "Day does not exist.";
+		  String actualMessage   = exception.getMessage();
+		  
+		  assertTrue(actualMessage.contains(expectedMessage));
+	  }
 
 }
