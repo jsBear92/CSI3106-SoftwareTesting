92e2c6cab92d1f4ea3c53303d5ae5677004da890
mikebernal <34907757+mikebernal@users.noreply.github.com>
2022-04-19 04:02:06 +0800

    change month from 10 to 12 at testInvalidMonth

--- tests/ScheduleMeetingTest.java
+++ tests/ScheduleMeetingTest.java
@@ -13,13 +13,21 @@ import au.edu.sccs.csp3105.NBookingPlanner.Room;
 @ExtendWith(RoomParameterResolver.class)
 @DisplayName("Schedule a meeting test")
 public class ScheduleMeetingTest {
-	  @Test
-	  @DisplayName("Day is greater than 30")
-	  public void testIncorrectDay(Room where) {
-		  Exception exception = assertThrows(ConflictsException.class, ()-> where.isBusy(5, 530, 3, 4));
-		  String expectedMessage = "Day does not exist.";
-		  String actualMessage   = exception.getMessage();
-		  assertTrue(actualMessage.contains(expectedMessage));
-	  }
+	@Test
+	@DisplayName("Month is greater than 12")
+	public void testInvalidMonth(Room room) {
+		assertThrows(ConflictsException.class, ()-> room.isBusy(12, 25, 4, 5));
+	}
+	
+    @Test
+    @DisplayName("Day is greater than 30")
+    public void testInvalidDay(Room room) {
+	    Exception exception = assertThrows(ConflictsException.class, ()-> room.isBusy(5, 530, 3, 4));
+	    String expectedMessage = "Day does not exist.";
+	    String actualMessage   = exception.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
+    }
+    
+    
 
 }
