3986e5b41c893c7da2205bc030d7154e9359d5be
mikebernal <34907757+mikebernal@users.noreply.github.com>
2022-04-20 02:23:02 +0800

    added testValid day using BVA and EQ

--- tests/ScheduleMeetingTest.java
+++ tests/ScheduleMeetingTest.java
@@ -1,12 +1,17 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import au.edu.sccs.csp3105.NBookingPlanner.ConflictsException;
 import au.edu.sccs.csp3105.NBookingPlanner.Room;
@@ -15,20 +20,32 @@ import au.edu.sccs.csp3105.NBookingPlanner.Room;
 @DisplayName("Schedule a meeting test")
 public class ScheduleMeetingTest {
 	@Test
-	@DisplayName("Month is less than or equal to 12")
+	@DisplayName("Month does not exists.")
 	public void testValidMonth(Room room) {
 		Exception exception = assertThrows(ConflictsException.class, ()-> room.isBusy(12, 25, 4, 5));
+		String expectedMessage = "Month does not exist.";
 	    String actualMessage   = exception.getMessage();
-	    assertEquals("", actualMessage);
+	    assertTrue(actualMessage.contains(expectedMessage));
 	}
 	
     @Test
-    @DisplayName("Day is greater than 30")
+    @DisplayName("Day does not exists.")
     public void testInvalidDay(Room room) {
 	    Exception exception = assertThrows(ConflictsException.class, ()-> room.isBusy(5, 530, 3, 4));
 	    String expectedMessage = "Day does not exist.";
 	    String actualMessage   = exception.getMessage();
 	    assertTrue(actualMessage.contains(expectedMessage));
+    }
+ 
+    @ParameterizedTest
+    @MethodSource("range")
+    @DisplayName("Accepts day 1 to 30")
+    public void testValidDay(int day) {
+      	assertTrue((day >= 1) && (day <= 30));
+    }
+    
+    static IntStream range() {
+    	return IntStream.range(0, 101);
     }
     
     
