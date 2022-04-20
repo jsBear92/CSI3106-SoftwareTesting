5b7da6905e658ffd670e7b3819c0fca3c5a32b13
mikebernal <34907757+mikebernal@users.noreply.github.com>
2022-04-20 15:43:35 +0800

    added calendar DI + unit test for invalid room

---
+++ tests/OrganizationParameterResolver.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+import au.edu.sccs.csp3105.NBookingPlanner.Organization;
+
+public class OrganizationParameterResolver implements ParameterResolver {
+	
+	@Override
+	public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+		// TODO Auto-generated method stub
+		
+		return parameterContext.getParameter().getType() == Organization.class;
+	}
+
+	@Override
+	public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+		// TODO Auto-generated method stub
+		return new Organization();
+	}
+
+}
--- tests/ScheduleMeetingTest.java
+++ tests/ScheduleMeetingTest.java
@@ -2,24 +2,32 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Arrays;
+import java.util.stream.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import au.edu.sccs.csp3105.NBookingPlanner.ConflictsException;
+import au.edu.sccs.csp3105.NBookingPlanner.Organization;
 import au.edu.sccs.csp3105.NBookingPlanner.Room;
+import net.bytebuddy.asm.Advice.OffsetMapping.Target.ForArray;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(OrganizationParameterResolver.class)
 @ExtendWith(CalendarParameterResolver.class)
 @ExtendWith(RoomParameterResolver.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DisplayName("Schedule a meeting test")
 public class ScheduleMeetingTest {
-	
 
 	@Order(1)
     @ParameterizedTest
@@ -63,5 +71,27 @@ public class ScheduleMeetingTest {
 	    String expectedMessage = "Illegal hour.";
 	    String actualMessage   = exception.getMessage();
 	    assertTrue(actualMessage.contains(expectedMessage));
-    } 
+    }
+	
+	@Order(5)
+	@Test
+//    @ParameterizedTest
+//    @CsvFileSource("")
+//    @ValueSource(strings = { "JO18.330", "JO7.221" })
+    @DisplayName("EQ / BVA + Exception: Room")
+    public void testInvalidRoom(Organization org) {
+		String[] rooms = { "ICT30.300" };
+		
+		Stream<String> stream = Arrays.stream(rooms);
+		stream.forEach(room -> {
+			try {
+				assertTrue(room == org.getRoom(room).getID());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				fail();
+			}
+		});
+		
+    }
 }
