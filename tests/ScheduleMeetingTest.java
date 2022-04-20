c5663fc3b8d3657a3cb12642095acaf9472b7984
mikebernal <34907757+mikebernal@users.noreply.github.com>
2022-04-20 08:47:16 +0800

    modify exception

--- tests/ScheduleMeetingTest.java
+++ tests/ScheduleMeetingTest.java
@@ -3,51 +3,47 @@ package tests;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import au.edu.sccs.csp3105.NBookingPlanner.Calendar;
 import au.edu.sccs.csp3105.NBookingPlanner.ConflictsException;
 import au.edu.sccs.csp3105.NBookingPlanner.Room;
 
+@ExtendWith(CalendarParameterResolver.class)
 @ExtendWith(RoomParameterResolver.class)
 @DisplayName("Schedule a meeting test")
 public class ScheduleMeetingTest {
-	@Test
-	@DisplayName("Month does not exists.")
-	public void testValidMonth(Room room) {
-		Exception exception = assertThrows(ConflictsException.class, ()-> room.isBusy(12, 25, 4, 5));
-		String expectedMessage = "Month does not exist.";
-	    String actualMessage   = exception.getMessage();
-	    assertTrue(actualMessage.contains(expectedMessage));
-	}
-	
-    @Test
-    @DisplayName("Day does not exists.")
-    public void testInvalidDay(Room room) {
-	    Exception exception = assertThrows(ConflictsException.class, ()-> room.isBusy(5, 530, 3, 4));
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 30, 31, 32 })
+    @DisplayName("EQ & BVA Day input testing")
+    public void testInvalidDay(int day, Room room) {
+	    Exception exception = assertThrows(ConflictsException.class, ()-> room.isBusy(5, day, 3, 4));
 	    String expectedMessage = "Day does not exist.";
 	    String actualMessage   = exception.getMessage();
 	    assertTrue(actualMessage.contains(expectedMessage));
     }
- 
+    
     @ParameterizedTest
-    @MethodSource("range")
-    @DisplayName("Accepts day 1 to 30")
-    public void testValidDay(int day) {
-      	assertTrue((day >= 1) && (day <= 30));
+    @ValueSource(ints = {0, 1, 2, 12, 13 })
+    @DisplayName("EQ & BVA Month input testing")
+    public void testInvalidMonth(int month, Room room) {
+	    Exception exception = assertThrows(ConflictsException.class, ()-> room.isBusy(month, 25, 3, 4));
+	    String expectedMessage = "Month does not exist.";
+	    String actualMessage   = exception.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
     }
     
-    static IntStream range() {
-    	return IntStream.range(0, 101);
-    }
     
-    
-
 }
---
+++ tests/UtilityTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+import java.util.stream.IntStream;
+
+public class UtilityTest {
+	public static IntStream range() {
+		return IntStream.range(0, 32);
+	}
+
+}
