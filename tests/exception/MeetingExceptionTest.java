12e99ee429f92d8d7fba6c1a55a9c7506e626302
mikebernal <34907757+mikebernal@users.noreply.github.com>
2022-04-22 17:20:07 +0800

    added comments

--- tests/exception/MeetingExceptionTest.java
+++ tests/exception/MeetingExceptionTest.java
@@ -1,8 +1,11 @@
 package tests.exception;
 
+// to do: use assertions.assertThrow to check what exception is thrown
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+
 
 public class MeetingExceptionTest {
 
--- tests/structural/MeetingStructuralTest.java
+++ tests/structural/MeetingStructuralTest.java
@@ -6,20 +6,39 @@ import org.junit.jupiter.api.Test;
 
 public class MeetingStructuralTest {
 	@Test
-	public void statementCoverage() {
+	public void testScPath1() {
 		// Traverse to one path here based on your CFG diagram
 		// Branch and condition coverage is not necessary if can be traverse in one path
 		assertTrue(true);
 	}
 	
 	@Test
-	public void branchCoverage1() {
+	public void testScPath2() {
+		// Traverse to one path here based on your CFG diagram
+		// Branch and condition coverage is not necessary if can be traverse in one path
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testBcPath1() {
+		// Traverse one path branch
+		assertTrue(true);
+	}
+
+	@Test
+	public void testBcPath2() {
 		// Traverse one path branch
 		assertTrue(true);
 	}
 	
 	@Test
-	public void conditionCoverage1() {
+	public void testCbPath1() {
+		// Traverse to one conditionBranch
+		assertTrue(true);
+	}
+
+	@Test
+	public void testCbPath2() {
 		// Traverse to one conditionBranch
 		assertTrue(true);
 	}
