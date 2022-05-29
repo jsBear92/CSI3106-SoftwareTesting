5a8eeadc5b1d1c584e03de43734e9897277429b4
mikebernal <34907757+mikebernal@users.noreply.github.com>
2022-05-30 05:08:07 +0800

    completed scpaths for isBusy()

--- tests/structural/CalendarIsBusyTest.java
+++ tests/structural/CalendarIsBusyTest.java
@@ -1,14 +1,10 @@
 package tests.structural;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,14 +12,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import au.edu.sccs.csp3105.NBookingPlanner.Calendar;
 import au.edu.sccs.csp3105.NBookingPlanner.ConflictsException;
 import au.edu.sccs.csp3105.NBookingPlanner.Meeting;
-import au.edu.sccs.csp3105.NBookingPlanner.Planner;
+import au.edu.sccs.csp3105.NBookingPlanner.Organization;
+import au.edu.sccs.csp3105.NBookingPlanner.Person;
+import au.edu.sccs.csp3105.NBookingPlanner.Room;
 import tests.resolver.CalendarParameterResolver;
 
-//Note
+// Note
 //
-//11, 30 error see Calendar.java at line 43
-//occupied.get(9).get(26).add(new Meeting(9, 26, "Apocalypse"));
-//System.out.println(occupied.get(month).get(day).get(0).getDescription());
+// 11, 30 error see Calendar.java at line 43
+// occupied.get(9).get(26).add(new Meeting(9, 26, "Apocalypse"));
+// System.out.println(occupied.get(month).get(day).get(0).getDescription());
 
 
 @ExtendWith(CalendarParameterResolver.class)
@@ -63,22 +61,22 @@ class CalendarIsBusyTest {
 	
 	// Event date  9, 26, 10, 11
 	// This unit test should pass since there is no event booked for  9, 26
-	public Boolean dateIsNotBusy() throws ConflictsException {
+	public Boolean testIsBusy(int month, int day, int start, int end) throws ConflictsException {
 		// 2.1
-		int month = 9;
-		int day   = 26;
-		int start = 10;
-		int end   = 11;
 		Boolean busy = false;
-		
+
 		// 2.2
 		Calendar.checkTimes(month, day, start, end);
 		
 		// 2.3
 		for(Meeting toCheck : occupied.get(month).get(day)) {
+			// 2.4 && 2.5
 			if(start >= toCheck.getStartTime() && start <= toCheck.getEndTime()){
+				// 2.6
 				busy=true;
+			 // 2.7 && 2.8
 			}else if(end >= toCheck.getStartTime() && end <= toCheck.getEndTime()){
+				// 2.9
 				busy=true;
 			}
 		}
@@ -90,10 +88,103 @@ class CalendarIsBusyTest {
 	
 	@Test
 	@DisplayName("SC Path 1: 2.1, 2.2, 2.3(F), 2.10, 2.11")
-	public void dateIsNotBusyDriver() {
+	public void testSCPath1() {
 		CalendarIsBusyTest SCPath1 = new CalendarIsBusyTest();
 		try {
-			System.out.println(SCPath1.dateIsNotBusy());
+			System.out.println("SC Path 1: 2.1, 2.2, 2.3(F), 2.10, 2.11");
+			System.out.println("Class and method under test: Calendar.isBusy()");
+			System.out.println("Input: 9, 26, 10, 11");
+			System.out.println("busy: " + SCPath1.testIsBusy(9, 26, 10, 11));
+			System.out.println("===============================================================");
+		} catch (ConflictsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+	@Test
+	@DisplayName("SC Path 2: 2.1, 2.2, 2.3(T), 2.4(T), 2.5(T), 2.6, 2.3(F), 2.10, 2.11")
+	public void testSCPath2() {
+		ArrayList<Person> attendees = new ArrayList<Person>();
+		CalendarIsBusyTest SCPath2 = new CalendarIsBusyTest();
+		Room room = new Room();
+		Organization org = new Organization();
+		
+		String where = "JO18.330";
+		
+		try {
+			room = org.getRoom(where);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		Person a1 = null;
+		Person a2 = null;
+		
+		try {
+			a1 = org.getEmployee("Ashley Martin");
+			a2 = org.getEmployee("Mark Colin");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			attendees.add(a1);
+			attendees.add(a2);
+			
+			// Add new event at month(9), day(26) with start time of 8 and end time of 8
+			occupied.get(9).get(26).add(new Meeting(9, 26, 8, 9, attendees, room, "Apocalypse: The day of reckoning"));
+			System.out.println("SC Path 2: 2.1, 2.2, 2.3(T), 2.4(T), 2.5(T), 2.6, 2.3(F), 2.10, 2.11");
+			System.out.println("Class and method under test: Calendar.isBusy()");
+			System.out.println("Description: Test if start (9) is greater than or equal to new date start & less than or equal to new date end time");
+			System.out.println("Input: 9, 26, 9, 7");
+			System.out.println("busy: " + SCPath2.testIsBusy(9, 26, 9, 10));
+			System.out.println("===============================================================");
+		} catch (ConflictsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	@DisplayName("SC Path 3: 2.1, 2.2, 2.3(T), 2.4(F), 2.7(T), 2.8(T), 2.9, 2.3, 2.10, 2.11")
+	public void testSCPath3() {
+		ArrayList<Person> attendees = new ArrayList<Person>();
+		CalendarIsBusyTest SCPath3 = new CalendarIsBusyTest();
+		Room room = new Room();
+		Organization org = new Organization();
+		
+		String where = "JO18.330";
+		
+		try {
+			room = org.getRoom(where);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		Person a1 = null;
+		Person a2 = null;
+		
+		try {
+			a1 = org.getEmployee("Ashley Martin");
+			a2 = org.getEmployee("Mark Colin");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			attendees.add(a1);
+			attendees.add(a2);
+			
+			// Add new event at month(9), day(26) with start time of 8 and end time of 8
+			occupied.get(9).get(26).add(new Meeting(9, 26, 8, 9, attendees, room, "Apocalypse: The day of reckoning"));
+			System.out.println("SC Path 3: 2.1, 2.2, 2.3(T), 2.4(F), 2.7(T), 2.8(T), 2.9, 2.3, 2.10, 2.11");
+			System.out.println("Class and method under test: Calendar.isBusy()");
+			System.out.println("Description: Test if end (9) is greater than or equal to new date start & less than or equal to new date end time");
+			System.out.println("Input: 9, 26, 8, 10");
+			System.out.println("busy: " + SCPath3.testIsBusy(9, 26, 8, 9));
+			System.out.println("===============================================================");
 		} catch (ConflictsException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
